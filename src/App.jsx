7b1a5c799e1e0c6f5419efb83030ae127dd9ce7b
Deepak Singh <deepaksingh101101@gmail.com>
2024-08-10 19:50:32 +0530

    navbar done

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 import Home from './pages/home/Home'
+import Navbar from './components/navbar/Navbar'
 
 function App() {
 
   return (
     <BrowserRouter>
+          <Navbar/>
     <Routes>
       <Route path="/" element={<Home />}>
       </Route>
---
+++ src/components/navbar/Navbar.jsx
@@ -0,0 +1,19 @@
+import logo from '../../assets/images/bpglogo.webp'
+import './Navbar.css'
+
+export default function Navbar() {
+  return (
+    <div className="bg-[#F4FAFF]">
+      <div className='flex justify-between lg:px-[30px] md:px-[30px] sm:px-[25px] px-[5px] py-5'>
+        <img 
+          src={logo} 
+          className=' logo w-[270px] h-[39.24px] xs:w-[126px] xs:h-[30px]' 
+          alt='Logo' 
+        />
+        <button className='continueBtnShadow bg-black text-white px-[26px] py-[9px] font-semibold rounded-xl hover:bg-[#2cc1ff]'>
+          Continue
+        </button>
+      </div>
+    </div>
+  )
+}
