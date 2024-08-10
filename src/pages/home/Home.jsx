0e1bc5096b0ec82f35b7e4ccd96f25b405ea1648
Deepak Singh <deepaksingh101101@gmail.com>
2024-08-10 20:28:12 +0530

    giving to shivam

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,21 @@
 
+import { HiMiniLanguage } from "react-icons/hi2";
+import { Link } from "react-router-dom";
+
 export default function Home() {
   return (
-    <div>Home</div>
+    <div className="w-full" >
+
+<div className="languageSelector flex  mt-10 justify-center items-center ">
+<HiMiniLanguage   className="h-[23px] text-[#51677B] me-2 w-[23px]" />
+<Link className="underline text-[#51677B]" >change language</Link>
+</div>
+
+
+<div className="flex justify-center">
+<h1 style={{lineHeight:"80px"}} className="text-[68px]  font-medium text-center mt-8 max-w-[1060px] w-full"   >World's Best AI Business Plan Generator</h1>
+
+</div>
+    </div>
   )
 }
