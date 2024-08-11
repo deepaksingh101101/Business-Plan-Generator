1a9cb5b06849f2f82360e3f2f0ceccd9d624b925
SHIVAM KUMAR <shivamjha2705@gmail.com>
2024-08-11 13:51:29 +0530

    updated review section

--- src/components/customerReview/Review.jsx
+++ src/components/customerReview/Review.jsx
@@ -1,18 +1,13 @@
-import chat1 from '../../assets/images/chat1.webp';
-import chat2 from '../../assets/images/chat2.webp';
-import chat3 from '../../assets/images/chat3.webp';
-import chat4 from '../../assets/images/chat4.webp';
-import chat5 from '../../assets/images/chat5.webp';
-import chat6 from '../../assets/images/chat6.webp';
+import { chat1,chat2,chat3,chat4,chat5,chat6 } from "../../assets/images";
 
 const Review = () => {
   return (
-    <div className="container mx-auto px-4 max-w-[1200px]">
-      <h1 className="text-start text-5xl font-medium mb-6">What customers say about us</h1>
+    <div className="container mx-auto px-4 max-w-[1300px]">
+      <h1 className="text-start text-[32px] lg:text-[54px] font-medium mb-6">What customers say about us</h1>
       <p className='text-xl text-slate-400'>Our ultimate goal is to make a product that you love, to do that we need feedback. So we try to talk to as many customers as we can</p>
       <div className="flex justify-center items-center py-10">
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
-          <div className="flex flex-col p-8 rounded-3xl bg-[rgba(230,241,250,0.5)] transition-shadow duration-300 ease-in-out shadow-lg hover:shadow-xl">
+          <div className="flex flex-col p-8 rounded-3xl bg-[rgba(230,241,250,0.5)] transition-shadow duration-300 ease-in-out hover:shadow-xl">
             <p className="text-lg md:text-2xl font-medium pb-4"><strong>Zede H.</strong></p>
             <div className="flex flex-col justify-center items-center space-y-4">
               <img 
@@ -34,7 +29,7 @@ const Review = () => {
             </div>
           </div>
 
-          <div className="flex flex-col p-8 rounded-3xl bg-[rgba(230,241,250,0.5)] transition-shadow duration-300 ease-in-out shadow-lg hover:shadow-xl">
+          <div className="flex flex-col p-8 rounded-3xl bg-[rgba(230,241,250,0.5)] transition-shadow duration-300 ease-in-out hover:shadow-xl">
             <p className="text-lg md:text-2xl font-medium pb-4"><strong>Jason C.</strong></p>
             <img 
               alt="Review chat 3" 
@@ -55,7 +50,7 @@ const Review = () => {
             />
           </div>
 
-          <div className="flex flex-col p-8 rounded-3xl bg-[rgba(230,241,250,0.5)] transition-shadow duration-300 ease-in-out shadow-lg hover:shadow-xl">
+          <div className="flex flex-col p-8 rounded-3xl bg-[rgba(230,241,250,0.5)] transition-shadow duration-300 ease-in-out hover:shadow-xl">
             <p className="text-lg md:text-2xl font-medium pb-4"><strong>Parker A.</strong></p>
             <img 
               alt="Review chat 5" 
